ffd67ec9c7eb0ac3442ea92fb895cc0cdd13b498
Chris Hannon <himynameschris@gmail.com>
2012-07-01 21:41:02 -0400

    GBox2DX Task: remove unused LUA modules from android makefile to speed up compile time

--- tests/proj.android/jni/Android.mk
+++ tests/proj.android/jni/Android.mk
@@ -9,11 +9,7 @@ LOCAL_MODULE_FILENAME := libgame
 LOCAL_SRC_FILES := helloworld/main.cpp \
                    ../../Classes/AppDelegate.cpp \
                    ../../Classes/TestScene.cpp \
-                   ../../Classes/TestScreenEdge.cpp \
-                   ../../../../lua/cocos2dx_support/CCLuaEngine.cpp \
-                                      ../../../../lua/cocos2dx_support/Cocos2dxLuaLoader.cpp \
-                                      ../../../../lua/cocos2dx_support/LuaCocos2d.cpp \
-                                      ../../../../lua/cocos2dx_support/tolua_fix.c
+                   ../../Classes/TestScreenEdge.cpp
                    
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
                     $(LOCAL_PATH)/../../../GBox2D \
@@ -23,4 +19,4 @@ LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static box2d_stati
             
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,CocosDenshion/android) $(call import-module,cocos2dx) $(call import-module,Box2D) $(call import-module,lua/proj.android/jni) $(call import-module,GBox2D/Classes)
+$(call import-module,CocosDenshion/android) $(call import-module,cocos2dx) $(call import-module,Box2D) $(call import-module,GBox2D/Classes)
